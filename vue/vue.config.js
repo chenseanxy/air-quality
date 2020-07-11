967e6a48fa12ac9ede52b693d2531c07ea857692
Chen Xinyang <chenxy1999728@126.com>
2020-07-11 23:13:58 +0800

    Frontend cleanups

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,5 +1,23 @@
 module.exports = {
     devServer: {
         proxy: 'http://localhost:8000'
-    }
+    },
+    // pwa: {
+    //     name: "Air Quality Checker",
+    //     themeColor: "#607D8B",
+    //     appleMobileWebAppCapable: 'yes',
+    //     appleMobileWebAppStatusBarStyle: 'black',
+    //     manifestOptions: {
+    //         icons: [{ 
+    //             'src': './img/icons/192x192.png', 
+    //             'sizes': '192x192', 
+    //             'type': 'image/png' 
+    //         }, { 
+    //             'src': './img/icons/512x512.png', 
+    //             'sizes': '512x512', 
+    //             'type': 'image/png' 
+    //         }]
+
+    //     }
+    // }
 }
